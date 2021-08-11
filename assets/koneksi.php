706ca5d222a555ac6603578dc0ef1aaff40e9c27
hamidseptian <56619322+hamidseptian@users.noreply.github.com>
2021-08-11 12:41:27 +0700

    Update koneksi.php

--- assets/koneksi.php
+++ assets/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-// error_reporting(0)	;
+error_reporting(0)	;
   // $conn = mysqli_connect('sql310.epizy.com','epiz_29021525','PIiJR0NKQG','epiz_29021525_fotografi');
   $conn = mysqli_connect('localhost','root','','fotografi');
   date_default_timezone_set('Asia/Jakarta');
